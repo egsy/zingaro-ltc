016edaf7172d1a7c4b440e902b54da7648f7697b
Elaine Yeung <31430673+egsy@users.noreply.github.com>
2022-02-28 13:25:49 -0600

    Add next solution

--- 02/2-4_dmopc16c1p0.py
+++ 02/2-4_dmopc16c1p0.py
@@ -1,2 +1,16 @@
-
 # https://dmoj.ca/problem/dmopc16c1p0
+
+ABSOLUTELY = "absolutely"
+FAIR = "fairly"
+VERY = "very"
+OUTPUT = "C.C. is M satisfied with her pizza."
+
+width = int(input())
+cheesiness = int(input())
+
+if width == 3 and cheesiness >= 95:
+    print(OUTPUT.replace("M", ABSOLUTELY))
+elif width == 1 and cheesiness <= 50:
+    print(OUTPUT.replace("M", FAIR))
+else:
+    print(OUTPUT.replace("M", VERY))
