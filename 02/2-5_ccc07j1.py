9566048eb932d163c01af835290842219a635d46
Elaine Yeung <31430673+egsy@users.noreply.github.com>
2022-02-28 13:53:09 -0600

    Add next solution

--- 02/2-5_ccc07j1.py
+++ 02/2-5_ccc07j1.py
@@ -1,1 +1,12 @@
 # https://dmoj.ca/problem/ccc07j1
+# Write a program that reads in three weights and
+# prints out the weight of Mama Bear's bowl (middle).
+# You may assume all weights are positive integers less than 100.
+
+bowl_A = int(input())
+bowl_B = int(input())
+bowl_C = int(input())
+
+list = [bowl_A, bowl_B, bowl_C]
+list.sort()
+print(list.pop(1))
