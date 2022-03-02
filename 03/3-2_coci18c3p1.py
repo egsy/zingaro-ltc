62984eae050d02acfe916cd2b86bdc9113619787
Elaine Yeung <31430673+egsy@users.noreply.github.com>
2022-03-01 18:06:11 -0600

    Add WIP

--- 03/3-2_coci18c3p1.py
+++ 03/3-2_coci18c3p1.py
@@ -1,11 +1,21 @@
 # https://dmoj.ca/problem/coci18c3p1
 # determine maximum number of times the string "HONI" appears in input
 
-# HONI = "HONI"
+HONI = "HONI"
 string = str(input())
 honi_count = 0
+char = 0
+letter = 0
 
 for char in range(len(string)):
+    for letter in range(len(HONI)):
+        if string[char] == HONI[letter]:
+            char = char + 1
+            letter = letter + 1
+        elif string[char] == HONI[letter] and letter == 3:
+            char = char + 1
+            letter = 0
+            honi_count = honi_count + 1
     # for i in range(len(HONI)):
     #     if i < 3 and string[char] == HONI[i]:
     #         i = i + 1
