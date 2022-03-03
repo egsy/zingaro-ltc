1f6fd9710876fe812ca09431fbadc7f158b6ab57
Elaine Yeung <31430673+egsy@users.noreply.github.com>
2022-03-03 16:33:21 -0600

    Update working solution

--- 03/3-2_coci18c3p1.py
+++ 03/3-2_coci18c3p1.py
@@ -8,20 +8,9 @@ char = 0
 letter = 0
 
 for char in range(len(string)):
-    for letter in range(len(HONI)):
-        if string[char] == HONI[letter]:
-            char = char + 1
-            letter = letter + 1
-        elif string[char] == HONI[letter] and letter == 3:
-            char = char + 1
+    if string[char] == HONI[letter]:
+        letter = letter + 1
+        if letter == 4:
             letter = 0
             honi_count = honi_count + 1
-    # for i in range(len(HONI)):
-    #     if i < 3 and string[char] == HONI[i]:
-    #         i = i + 1
-    #     elif i == 3 and string[char] == HONI[i]:
-    #         honi_count = honi_count + 1
-    #     elif i > 3:
-    #         i = 0
-
 print(honi_count)
