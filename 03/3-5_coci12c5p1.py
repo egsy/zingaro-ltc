04f1ab6bb1fa7f22d818f18deb5d462db0c56b45
Elaine Yeung <31430673+egsy@users.noreply.github.com>
2022-03-04 01:08:56 -0600

    Add solution

--- 03/3-5_coci12c5p1.py
+++ 03/3-5_coci12c5p1.py
@@ -1,5 +1,5 @@
 # https://dmoj.ca/problem/coci12c5p1
-# write a program to determin if a composition is more likely written in
+# write a program to determine if a composition is more likely written in
 # A-minor or C-major
 
 A_MIN = "A-mol"
@@ -12,23 +12,26 @@ composition = input(str())
 accented = composition[0]
 
 for char in range(len(composition)):
-    if composition[char] != "|":
-        continue
-    else:
+    if composition[char] == "|":
         accented = accented + composition[char + 1]
+    elif composition[char] != "|":
+        continue
 
 a_min_count = 0
 c_maj_count = 0
-print(accented)
+
 for note in range(len(accented)):
     if accented[note] in a_min_tones:
         a_min_count = a_min_count + 1
-    elif accented[note] in c_maj_tones:
+    if accented[note] in c_maj_tones:
         c_maj_count = c_maj_count + 1
 
 if a_min_count > c_maj_count:
     print(A_MIN)
 elif a_min_count < c_maj_count:
     print(C_MAJ)
-else:
-    print("oh no same amount")
+elif a_min_count == c_maj_count:
+    if accented[-1] == "C":
+        print(C_MAJ)
+    elif accented[-1] == "A":
+        print(A_MIN)
