0c9dbc89107e8fb1366d68ac6427e243fe529fdd
Elaine Yeung <31430673+egsy@users.noreply.github.com>
2022-03-16 12:34:53 -0500

    Update 4-1 to use modulo

--- 04/slot_ccc00s1.py
+++ 04/slot_ccc00s1.py
@@ -7,31 +7,24 @@ second = int(input())
 third = int(input())
 
 plays = 0
-machine = 0
 
 while quarters >= 1:
+    machine = plays % 3
     quarters -= 1
 
     if machine == 0:
         first += 1
-        if first == 35:
-            first = 0
+        if first % 35 == 0:
             quarters += 30
     elif machine == 1:
         second += 1
-        if second == 100:
-            second = 0
+        if second % 100 == 0:
             quarters += 60
     elif machine == 2:
         third += 1
-        if third == 10:
-            third = 0
+        if third % 10 == 0:
             quarters += 9
 
     plays += 1
-    machine += 1
-
-    if machine == 3:
-        machine = 0
 
 print(output.format(plays))
