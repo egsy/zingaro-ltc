e12b5ff5bc97d3dedbe8baaf51ac623c911d9abb
Elaine Yeung <31430673+egsy@users.noreply.github.com>
2022-03-16 11:57:33 -0500

    Add solution

--- 03/3-7_coci18c4p1.py
+++ 03/3-7_coci18c4p1.py
@@ -1,1 +1,17 @@
 # https://dmoj.ca/problem/coci18c4p1
+
+elder = str(input())
+duel_count = int(input())
+wizard_count = 1
+wizard_history = elder
+
+for i in range(duel_count):
+    duel = str(input())
+    if duel[-1] == elder:
+        elder = duel[-3]
+        if elder not in wizard_history:
+            wizard_history += duel[-1]
+            wizard_count = wizard_count + 1
+
+print(elder)
+print(wizard_count)
