5d5d15f53ccb7b7a0d536c44aedebd1becc9392e
Elaine Yeung <31430673+egsy@users.noreply.github.com>
2022-03-19 19:01:16 -0500

    Add WIP

--- 04/4-4_ecoo13r1p1.py
+++ 04/4-4_ecoo13r1p1.py
@@ -1,1 +1,19 @@
 # DMOJ problem ecoo13r1p1, Take a Number
+
+current = int(input())
+status = ""
+i = 0
+j = 0
+output = "{} {} {}"
+while status != "EOF":
+    status = input(str())
+    if status == "TAKE":
+        i += 1
+    elif status == "SERVE":
+        j += 1
+    elif status == "CLOSE":
+        # print(current)
+        current = current + i
+        print(output.format(i, (i - j), current))
+        i = 0
+        j = 0
