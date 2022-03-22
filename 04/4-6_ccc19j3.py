cb714f749ee8f6f059f3a8c0228bfadb9eb91376
Elaine Yeung <31430673+egsy@users.noreply.github.com>
2022-03-22 18:49:44 -0500

    WIP run length encoding algorithm

--- 04/4-6_ccc19j3.py
+++ 04/4-6_ccc19j3.py
@@ -1,1 +1,19 @@
+# https://dmoj.ca/problem/ccc19j3
 # DMOJ problem ccc19j3, Cold Compress
+
+num = int(input())
+i = 0
+output = "{} {}"
+
+for line in range(num):
+    text = str(input())
+    while i < len(text):
+        # count how many times character at index `i` appears
+        count = 1
+
+        while i + 1 < len(text) and text[i] == text[i + 1]:
+            count += 1
+            i += 1
+        print(output.format(count, text[i]))
+        i += 1
+    line += 1
