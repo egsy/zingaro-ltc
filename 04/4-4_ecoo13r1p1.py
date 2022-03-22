3c29f1a6e0723fdb2dfb0b38b80593893ead3019
Elaine Yeung <31430673+egsy@users.noreply.github.com>
2022-03-22 13:16:57 -0500

    Add last criteria for problem

--- 04/4-4_ecoo13r1p1.py
+++ 04/4-4_ecoo13r1p1.py
@@ -9,11 +9,14 @@ while status != "EOF":
     status = input(str())
     if status == "TAKE":
         i += 1
+        if current == 1000:
+            current = 1
+            current += 1
+        else:
+            current += 1
     elif status == "SERVE":
         j += 1
     elif status == "CLOSE":
-        # print(current)
-        current = current + i
         print(output.format(i, (i - j), current))
         i = 0
         j = 0
