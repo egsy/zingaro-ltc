ff7c9e7272a5e5342937363bc47005c98a69737d
Elaine Yeung <31430673+egsy@users.noreply.github.com>
2022-03-26 19:29:12 -0500

    Update with solutions

--- 05/5-5_ecoo18r1p1.py
+++ 05/5-5_ecoo18r1p1.py
@@ -1,18 +1,17 @@
 # ecoo18r1p1 ECOO '18 R1 P1 - Willow's Wild Ride
 for dataset in range(10):
     info = input().split()
-    playtime = int(info[0])
-    days = int(info[1])
-    box = []
+    t = int(info[0])
+    n = int(info[1])
+    box_time = 0
 
-    for day in range(days):
-        box.append(input())
+    for day in range(n):
+        day = input()
+        if day == "B":
+            box_time += t
+            box_time -= 1
+        else:
+            if box_time > 0:
+                box_time -= 1
 
-    playtime = box.count("B") * playtime
-    days = len(box[box.index("B") :])
-
-    # if playtime > days:
-    # output = playtime - days
-    print(playtime - days)
-    # # else:
-    #     print(0)
+    print(box_time)
