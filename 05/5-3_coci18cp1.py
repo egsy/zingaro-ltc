031a730639028e244c659113a50608bab00039dc
Elaine Yeung <31430673+egsy@users.noreply.github.com>
2022-03-29 15:00:57 -0500

    WIP 5-3

--- 05/5-3_coci18cp1.py
+++ 05/5-3_coci18cp1.py
@@ -4,21 +4,19 @@ A = int(input())
 As = []
 for num in range(A):
     As.append(int(input()))
-print(As)
+
 B = int(input())
 Bs = []
 for num in range(B):
     Bs.append(int(input()))
-print(Bs)
+
 # calculate how many points scored in first half-time
 # game lasts 4x12 min, so half game = 24 min = 1440 sec
 points = 0
 for sec in range(0, 1441):
     if As.count(sec) == 1:
-        print(sec)
         points += 1
     if Bs.count(sec) == 1:
-        print(sec)
         points += 1
 print(points)
 
@@ -34,12 +32,13 @@ for sec in range(0, 2881):
     if Bs.count(sec) == 1:
         score[1] += 1
 
-    if score[0] > score[1]:
+    if score[0] != 1 and score[0] > score[1]:
         lead = "A"
-    elif score[1] > score[0]:
+    elif score[1] != 1 and score[1] > score[0]:
         lead = "B"
 
     if lead != prev_lead:
         turnaround.append(True)
+
         print(score, "lead: ", lead)
         print(turnaround, turnaround.count(True))
