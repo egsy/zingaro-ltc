dc9c33f019ae689376c21530debc3e24a070a60e
Elaine Yeung <31430673+egsy@users.noreply.github.com>
2022-03-30 19:20:24 -0500

    WIP - not matching expected output

--- 05/5-6_ecoo19r1p1.py
+++ 05/5-6_ecoo19r1p1.py
@@ -5,7 +5,6 @@ for i in range(3):
     for i in range(len(data)):
         data[i] = int(data[i])
 
-    print(data)
     # `N` - the initial number of shirts that Ian has,
     # `M` the number of events coming up, and
     # `D` the number of days, respectively.
@@ -21,28 +20,35 @@ for i in range(3):
     for i in range(len(event_days)):
         event_days[i] = int(event_days[i])
 
-    print(event_days)
-
     # For each dataset, output the number of times
     # Ian will do the laundry in the next D D days.
     wash_count = 0
     clean_shirt = int(N[0])
+    idx = 0
     for i in range(1, D + 1):
         if clean_shirt == 0:
             wash_count += 1
-            print(f"day {i} laundry day - washed {wash_count} times")
             clean_shirt = sum(N)
-            
-        if clean_shirt > 0:
-            print(f"{clean_shirt} clean shirts")
-            clean_shirt -= 1
+            print(f"day {i} laundry day - washed {wash_count} times")
 
-        if event_days.count(i) > 0:
-            print(f"day {i}")
-            # handle multiple events on one day
-            new_shirt = 1 * event_days.count(i)
-            N.append(new_shirt)
-            clean_shirt += new_shirt
-            print(f"{new_shirt} new shirt(s)! now {clean_shirt} clean shirts left")
+        clean_shirt -= 1
 
-    print("- - - - - -")
+        # if event_days.count(i) > 0:
+        #     print(f"day {i}")
+        #     # handle multiple events on one day
+        #     clean_shirt += 1 * event_days.count(i)
+        #     N.append(event_days.count(i))
+
+        # print(f"{new_shirt} new shirt(s)! now {clean_shirt} clean shirts left")
+
+        while idx < len(event_days) and event_days[idx] == i:
+            clean_shirt += 1
+            N.append(1)
+            idx += 1
+
+        # if event_days.count(i) > 0:
+        #     # handle multiple events on one day
+        #     new_shirt = 1 * event_days.count(i)
+        #     print(f"day {i} {new_shirt} new shirt(s)! {clean_shirt} shirts left")
+
+    print(wash_count)
