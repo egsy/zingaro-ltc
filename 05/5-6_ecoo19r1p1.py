2cf1d2347f1f8ad1788a2e762d84529766d906aa
Elaine Yeung <31430673+egsy@users.noreply.github.com>
2022-03-31 00:46:49 -0500

    Update with 5-6 solution

--- 05/5-6_ecoo19r1p1.py
+++ 05/5-6_ecoo19r1p1.py
@@ -1,5 +1,5 @@
 # /ecoo19r1p1 ECOO '19 R1 P1 - Free Shirts - DMOJ: Modern Online Judge
-for i in range(3):
+for i in range(10):
     data = input()
     data = data.split()
     for i in range(len(data)):
@@ -8,8 +8,7 @@ for i in range(3):
     # `N` - the initial number of shirts that Ian has,
     # `M` the number of events coming up, and
     # `D` the number of days, respectively.
-
-    N = [data[0]]
+    N = data[0]
     M = data[1]
     D = data[2]
 
@@ -20,35 +19,24 @@ for i in range(3):
     for i in range(len(event_days)):
         event_days[i] = int(event_days[i])
 
+    event_days.sort()
+
     # For each dataset, output the number of times
     # Ian will do the laundry in the next D D days.
     wash_count = 0
-    clean_shirt = int(N[0])
+    clean_shirt = N
     idx = 0
     for i in range(1, D + 1):
         if clean_shirt == 0:
             wash_count += 1
-            clean_shirt = sum(N)
-            print(f"day {i} laundry day - washed {wash_count} times")
+            clean_shirt = N
+            # print(f"day {i} laundry day - washed {wash_count} times")
 
         clean_shirt -= 1
 
-        # if event_days.count(i) > 0:
-        #     print(f"day {i}")
-        #     # handle multiple events on one day
-        #     clean_shirt += 1 * event_days.count(i)
-        #     N.append(event_days.count(i))
-
-        # print(f"{new_shirt} new shirt(s)! now {clean_shirt} clean shirts left")
-
         while idx < len(event_days) and event_days[idx] == i:
             clean_shirt += 1
-            N.append(1)
+            N += 1
             idx += 1
-
-        # if event_days.count(i) > 0:
-        #     # handle multiple events on one day
-        #     new_shirt = 1 * event_days.count(i)
-        #     print(f"day {i} {new_shirt} new shirt(s)! {clean_shirt} shirts left")
 
     print(wash_count)
